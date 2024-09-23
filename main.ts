561bad09b57da3cbe7e64659bc93508771345daa
Yonatan Zhenin <yonatan.jenin@mail.huji.ac.il>
2024-09-23 18:05:30 +0300

    cleaned some bloat, added a new command

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ const DEFAULT_SETTINGS: RomajiPluginSettings = {
 export default class RomajiPlugin extends Plugin {
 	settings: RomajiPluginSettings;
 	pluginIndicator: HTMLElement;
-	isPluginOn = true;
+	isPluginOn = false;
 
 	async onload() {
 		await this.loadSettings();
@@ -22,63 +22,25 @@ export default class RomajiPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		this.pluginIndicator = this.addStatusBarItem();
 		this.updateIndicator();
+		this.app.workspace.on('editor-change', (editor: Editor) => {
+			const cursor = editor.getCursor();
+			const wordStart = editor.getCursor('from');
+		});
 
-
-
-
-
-
-
-
-
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'live-translate-switch',
+			name: 'activate live translate',
 			callback: () => {
-				// opens up a new window
-				new SampleModal(this.app).open();
+				this.isPluginOn = !this.isPluginOn;
+				this.updateIndicator();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				// This will replace the current selection in the editor with the text 'Sample Editor Command'
-				// and log the current selection to the console
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// when pressing the status bar item, the plugin will toggle the isPluginOn variable
 		this.registerDomEvent(this.pluginIndicator, 'click', (evt: MouseEvent) => {
 			this.isPluginOn = !this.isPluginOn;
 			this.updateIndicator();
-			console.log(this.isPluginOn);
 		});
 
 
@@ -110,23 +72,30 @@ export default class RomajiPlugin extends Plugin {
 
 	}
 
+
+	// sets the setting to the default value
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	// saves the setting into a file named data.json
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	s = "Woah!";
+	constructor(app: App, s?: string) {
 		super(app);
+		if (s) {
+			this.s = s;
+		}
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText(this.s);
 	}
 
 	onClose() {
